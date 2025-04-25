05b7d72111826c7be1f42d3da514a1be52864ae3
Fegar <antartyx@gmail.com>
2025-04-25 11:56:50 +0700

    set docker

--- server/app/databases.py
+++ server/app/databases.py
@@ -5,7 +5,13 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASES = os.getenv("DATABASE_URL")
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+db_port = os.getenv("DB_PORT", "5432")
+db_host = os.getenv("DB_HOST", "localhost")
+
+DATABASES = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
 
 engine = create_engine(DATABASES)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- server/app/main.py
+++ server/app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from models.base import Base
 from routes import auth, song
 from databases import engine
+import sys
+print(sys.path) 
 
 app = FastAPI()
 app.include_router(auth.router, prefix="/auth", tags=["auth"])
